9f760e89383a06ef12eaf6667b016ec5f19a3472
zxzxzx0119 <zxzxzx@github.com>
2017-11-28 08:43:42 +0800

    d

--- 数据结构/并查集/poj1611   The Suspects .cpp
+++ 数据结构/并查集/poj1611   The Suspects .cpp
@@ -1,69 +1,56 @@
 //题目连接；http://poj.org/problem?id=1611
 //题目大意：病毒传染，可以通过一些社团接触
           //给出一些社团，问有多少人被感染
-//解题思路：并查集找出公共祖先，加入集合
+//解题思路：并查集找出公共祖先，加入集合,然后找出和0在同一个集合的元素个数，顺便用ranks记录秩以优化
 
 
-//顺便用ranks记录秩以优化
+
 #include <iostream>
 #define maxn 30001
 using namespace std;
 
-int father[maxn];
+int pa[maxn];
 int ranks[maxn];
 
-int find(int v)
-{
-	if (father[v] == v)
-		return v;
-	return father[v] = find(father[v]);
+int Find(int v){
+	if (pa[v] == v)return v;
+	return pa[v] = Find(pa[v]);
 }
 
-void join(int x, int y)
-{
-	int fx = find(x);
-	int fy = find(y);
+void Union(int x, int y){
+	int fx = Find(x);
+	int fy = Find(y);
 	if (fx == fy)return;
-	if (ranks[fx] < ranks[fy])
-	{
-		father[fx] = fy;
-	}
-	else
-	{
-		father[fy] = fx;
+	if (ranks[fx] < ranks[fy])//fy的高度大于fx
+		pa[fx] = fy;  //按秩合并的原理是使得树的高度尽量的小
+	else{
+		pa[fy] = fx;
 		if (ranks[fx] == ranks[fy])
 			ranks[fy]++;
 	}
 }
 
-int main()
-{
+int main(){
 	int n, m, k, x, y;
-	while (cin >> n >> m && (n || m))
-	{
+	while (cin >> n >> m && (n || m)){
 		int sum = 1;
-		for (int i = 0; i < n; i++)
-		{
-			father[i] = i;
+		for (int i = 0; i < n; i++){//初始化
+			pa[i] = i;
 			ranks[i] = 0;
 		}
-		for (int i = 0; i < m; i++)
-		{
-			cin >> k;
-			if (k >= 1)
-				cin >> x;
-			for (int j = 1; j < k; j++)
-			{
+		for (int i = 0; i < m; i++){
+			cin>>k;
+			if (k >= 1)cin >> x;
+			for (int j = 1; j < k; j++){
 				cin >> y;
-				join(x, y);
+				Union(x, y);//合并集合
 			}
 		}
-		for (int i = 1; i < n; i++)
-		{
-			if (find(0) == find(i))
+		for (int i = 1; i < n; i++){
+			if (Find(0) == Find(i))//和0在同一个集合的元素
 				sum++;
 		}
-		cout << sum << endl;
+		cout<<sum<<endl;
 	}
 	return 0;
 }
